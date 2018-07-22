8abd949df4d372fc721123541c854627c5165de6
PavelDremanovich <pavel@dremanovich.com>
2018-07-22 17:27:46 +0300

    Multithreading test AdamantApiWrapper.

---
+++ app/src/test/java/im/adamant/android/TestConstants.java
@@ -0,0 +1,9 @@
+package im.adamant.android;
+
+public interface TestConstants {
+    String testPublicKey = "";
+    String testPassPhrase = "";
+    String testSecretKey = "";
+    String testAddress = "";
+    long testBalance = 100_000_000_000L;
+}
